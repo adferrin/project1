445e52a1dc13334b10644f12e800d2d63f354f80
adferrin <adferrin@icloud.com>
2020-07-14 14:47:41 -0700

    V1 Rick & Morty API project

---
+++ js/script.js
@@ -0,0 +1,78 @@
+/*----- constants -----*/
+
+const baseURL = `https://rickandmortyapi.com/api/character/?name=`;
+
+
+/*----- app's state (variables) -----*/
+let rickData, userInput
+
+
+
+
+
+
+/*----- cached element references -----*/
+const $name = $('#name');
+const $origin = $('#origin');
+const $status = $('#status');
+const $reward = $('#reward');
+const $input = $("input[type='text']");
+const $modal = $('.modal');
+
+
+
+
+/*----- event listeners -----*/
+$('form').on("submit", handleGetData);
+
+
+
+
+
+/*----- functions -----*/
+  // initialize modal
+$modal.modal();
+const instance = M.Modal.getInstance($modal);
+
+
+function handleGetData(event) {
+    event.preventDefault();
+
+    userInput = $input.val();
+    $.ajax({
+       url: `https://rickandmortyapi.com/api/character/?name=${userInput}`
+    
+    }).then(
+        (data) => {
+            console.log("DATA IS ", data)
+            rickData = data;
+            render();
+        },
+        (error) => {
+            console.log("Error is ", error);
+        }
+    )
+}
+
+function generateHTML() {
+    return rickData.map(function(p) {
+        return `
+        <li class="collection-item red-text">
+            <div style="text-transform: capitalize;">${p.name}
+                <span data-url="${p.url}" class="secondary-content blue-text">
+                Detail
+                </span>
+            </div>
+        </li>`;
+    });
+}
+
+
+function render() {
+    $name.html("Name: " + rickData.results[0].name);
+    $origin.html("Planet of Origin: " + rickData.results[0].origin.name);
+    $status.html("Dead or Alive: " + rickData.results[0].status);
+    $reward.html("Reward: Please Message for actual price - MGMT");
+    $('img').attr('src', rickData.results[0].image);
+    instance.open();
+}
